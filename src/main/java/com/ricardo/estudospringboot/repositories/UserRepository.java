a39c7e47fda8d185fd39569f7b59ef5caf5904b3
Ricardodesouz <ricardodesouza360@gmail.com>
2025-02-08 13:38:18 -0300

    Created UserRepository

---
+++ src/main/java/com/ricardo/estudospringboot/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.ricardo.estudospringboot.repositories;
+
+import com.ricardo.estudospringboot.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
