c98c5017cdda3ee730402a7bbabcbf8122b4d3ec
Ricardodesouz <ricardodesouza360@gmail.com>
2025-02-08 10:14:22 -0300

    implement jpa in User

--- src/main/java/com/ricardo/estudospringboot/entities/User.java
+++ src/main/java/com/ricardo/estudospringboot/entities/User.java
@@ -1,8 +1,17 @@
 package com.ricardo.estudospringboot.entities;
 
-import java.util.Objects;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
 
+import java.util.Objects;
+@Entity
+@Table(name="tb_user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String email;
