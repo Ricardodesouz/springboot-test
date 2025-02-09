47d49a616634e9a325659a90c096809deabd3194
Ricardodesouz <ricardodesouza360@gmail.com>
2025-02-09 10:44:34 -0300

    Created UserServices

---
+++ src/main/java/com/ricardo/estudospringboot/services/UserServices.java
@@ -0,0 +1,25 @@
+package com.ricardo.estudospringboot.services;
+
+import com.ricardo.estudospringboot.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.ricardo.estudospringboot.entities.User;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServices {
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+    public User findById(Integer id){
+        Optional<User> user = userRepository.findById(id);
+        return user.get();
+
+    }
+
+}
