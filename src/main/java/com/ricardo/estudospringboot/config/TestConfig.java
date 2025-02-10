62f1a605bd2ff77ec8b2fb72c4b1212b1530099a
Ricardodesouz <ricardodesouza360@gmail.com>
2025-02-10 10:52:36 -0300

    Created OrderStatus enum

--- src/main/java/com/ricardo/estudospringboot/config/TestConfig.java
+++ src/main/java/com/ricardo/estudospringboot/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.ricardo.estudospringboot.config;
 
 import com.ricardo.estudospringboot.entities.Order;
 import com.ricardo.estudospringboot.entities.User;
+import com.ricardo.estudospringboot.entities.enums.OrderStatus;
 import com.ricardo.estudospringboot.repositories.OrderRespository;
 import com.ricardo.estudospringboot.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,9 +28,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null,"ricardo","ricardo@gmail.com", "123456");
         User u2 = new User(null,"raquel","raquel@gmail.com", "989089");
         userRepository.saveAll(Arrays.asList(u1,u2));
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1, OrderStatus.PAID);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2,OrderStatus.PAID);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1, OrderStatus.CANCELED);
         orderRespository.saveAll(Arrays.asList(o1,o2,o3));
 
 
--- src/main/java/com/ricardo/estudospringboot/entities/Order.java
+++ src/main/java/com/ricardo/estudospringboot/entities/Order.java
@@ -1,5 +1,6 @@
 package com.ricardo.estudospringboot.entities;
 
+import com.ricardo.estudospringboot.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.time.Instant;
@@ -11,18 +12,23 @@ public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     @Column
     private Instant moment;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
 
+    private Integer orderStatus;
+
     public Order(){}
 
-    public Order(Integer id, Instant moment, User client) {
+    public Order(Integer id, Instant moment, User client, OrderStatus orderStatus) {
         this.id = id;
         this.moment = moment;
         this.client = client;
+        setOrderSatus(orderStatus);
     }
 
     public Integer getId() {
@@ -47,6 +53,15 @@ public class Order {
 
     public void setClient(User client) {
         this.client = client;
+    }
+    public OrderStatus getOrderStatus(){
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderSatus(OrderStatus orderSatus){
+        if(orderSatus != null) {
+            this.orderStatus = orderSatus.getCode();
+        }
     }
 
     @Override
